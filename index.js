adec987632d0f5721c32b6a849d14eed973c089b
Rachel Weisberger <rachel.weisberger@gmail.com>
2022-12-26 21:10:19 -0500

    activating serve static files

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ app.use(cors());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
-// app.use(express.static('public'));
+app.use(express.static('public'));
 
 const port = process.env.PORT || 4000;
 
