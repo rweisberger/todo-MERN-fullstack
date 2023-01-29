dfc7d1fab2b00506ed27ce9aba23bf94dd29f4ae
Rachel Weisberger <rachel.weisberger@gmail.com>
2023-01-29 06:52:33 -0500

    serving static files

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ app.use(cors());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
-// app.use(express.static('public'));
+app.use(express.static('public'));
 
 const port = process.env.PORT || 4000;
 const user = process.env.MONGO_USER;
